56dcfa370fa1f99a258bed7df3ef9d5de3ff0d9b
SiphoChris <siphonkebe@gmail.com>
2024-08-21 16:25:56 +0200

    home, about, and contact pages done

---
+++ src/components/TableComp.vue
@@ -0,0 +1,50 @@
+<template>
+    <h2>Products</h2>
+    <div class="table-responsive">
+      <table class="table table-hover table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <br>
+    <br>
+    <h2>Users</h2>
+    <div class="table-responsive">
+      <table class="table table-hover table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,17 +1,38 @@
 <template>
-    <div class="admin">
-      <Footer />
+  <div class="admin">
+    <div class="container">
+      <h1>Admin</h1>
+      <TableComp />
     </div>
-  </template>
-  <script>
-  import Footer from "@/components/FooterComp.vue";
-  export default {
-    name: 'AdminView',
-    components: {
-    Footer
-  }
-  }
-  </script>
-  <style scoped>
-  </style>
-  
+    <Footer id="footer" />
+  </div>
+</template>
+<script>
+import Footer from "@/components/FooterComp.vue";
+import TableComp from "@/components/TableComp.vue";
+export default {
+  name: "AdminView",
+  components: {
+    TableComp,
+    Footer,
+  },
+};
+</script>
+<style scoped>
+
+h1{
+    margin: 5rem 0;
+    text-decoration: underline;
+}
+.admin{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 5rem;
+}
+#footer{
+  width: 100%;
+}
+
+</style>
